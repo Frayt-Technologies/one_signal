bfd9bb42852f0156c99334bb1eb6006cd238ea6f
Michael M. Rosario <mchl0208@gmail.com>
2024-08-17 14:22:28 -0400

    MAR-1113: Refactoring the `Notification` module

--- lib/one_signal/notification.ex
+++ lib/one_signal/notification.ex
@@ -1,27 +1,59 @@
 defmodule OneSignal.Notification do
-  defstruct id: nil, recipients: 0, errors: %{}
+  use OneSignal.Entity
+  import OneSignal.Request
 
-  def post_notification_url() do
-    OneSignal.endpoint() <> "/notifications"
-  end
+  @plural_endpoint "/notifications"
 
-  @doc """
-  Send push notification
-  iex> OneSignal.Notification.send(%{"en" => "Hello!", "ja" => "はろー"}, %{"included_segments" => ["All"], "isAndroid" => true})
-  """
+  @type target_channel :: :email | :sms | :push
 
-  def send(body) do
-    case OneSignal.API.post(post_notification_url(), body) do
-      {:ok, response} ->
-        response = Enum.map(response, &to_key_atom/1)
-        struct(__MODULE__, response)
+  @type t :: %__MODULE__{
+          included_segments: list(string) | nil,
+          excluded_segments: list(string) | nil,
+          include_email_tokens: list(string) | nil,
+          include_phone_numbers: list(string) | nil,
+          # filters: list(OneSignal.Filter) | nil,
+          include_aliases:
+            %{
+              external_id: list(string) | nil,
+              onesignal_id: list(string) | nil,
+              some_custom_alias: list(string) | nil
+            }
+            | nil,
+          include_subscription_ids: list(string) | nil,
+          target_channel: target_channel,
+          custom_data: map() | nil,
+          template_id: String.t() | nil,
+          contents: %{
+            en: String.t()
+          },
+          headings: %{
+            en: String.t()
+          },
+          url: String.t() | nil
+        }
 
-      err ->
-        err
-    end
-  end
+  defstruct [
+    :included_segments,
+    :excluded_segments,
+    :include_email_tokens,
+    :include_phone_numbers,
+    # :filters,
+    :include_aliases,
+    :include_subscription_ids,
+    :target_channel,
+    :custom_data,
+    :template_id,
+    :contents,
+    :headings,
+    :url
+  ]
 
-  def to_key_atom({k, v}) do
-    {String.to_atom(k), v}
+  @spec create(t, OneSignal.options()) :: {:ok, nil} | {:error, OneSignal.Error.t()}
+  def create(params, opt \\ []) do
+    new_request(opt)
+    |> put_endpoint(@plural_endpoint)
+    |> put_method(:post)
+    |> put_params(params)
+    |> make_request()
   end
 end
